48cf42e9a9256714bc52c484fc76c8a4ca7ce7d9
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-09 12:22:15 -0500

    feat(validation): card, cvc, expire date

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -24,9 +24,9 @@ const Confirm = ({ state, setState, paymentMethod }) => {
         <Fragment>
             <CSSTransition in={state} timeout={300} classNames="dialog">
                 <Modal closeTimeoutMS={500} isOpen={state} style={modalStyles}>
+                    <h3>Sure you want to proceed with payment?</h3>
                     <button onClick={setModalState}>Cancel</button>
                     <button onClick={handleTransaction}>Confirm Payment</button>
-                    <div>Hello World</div>
                 </Modal>
             </CSSTransition>
             {confirm ? (
--- src/components/payment/credit/CardForm.js
+++ src/components/payment/credit/CardForm.js
@@ -1,18 +1,41 @@
-import React, { Fragment } from 'react'
-// import OpenPay from '../../../classes/OpenPay'
+import React, { Fragment, useState } from 'react'
+import OpenPay from '../../../classes/OpenPay'
 
 const CardForm = ({ continuePayment, emptyInputs }) => {
+    const [card, setCard] = useState(true)
+    const [cvc, setCvc] = useState(true)
+    const [date, setDate] = useState(true)
+
     const validateInputs = () => {
-        if (!emptyInputs()) {
+        var card = validCard()
+        var cvc = validCvc()
+        var date = validDate()
+        if (!emptyInputs() && card && cvc && date) {
             continuePayment()
         }
     }
 
-    // const validCard = () => {}
+    const validCard = () => {
+        var card = document.getElementById('card').value
+        var state = OpenPay.card.validateCardNumber(card) //openpay validates if card number is valid and returns answer
+        setCard(state)
+        return state
+    }
 
-    // const validCvc = () => {}
+    const validCvc = () => {
+        var cvc = document.getElementById('cvc').value
+        var state = OpenPay.card.validateCVC(cvc) //openpay validates if card number is valid and returns answer
+        setCvc(state)
+        return state
+    }
 
-    // const validDate = () => {}
+    const validDate = () => {
+        var month = document.getElementById('month').value
+        var year = document.getElementById('year').value
+        var state = OpenPay.card.validateExpiry(month, year) //openpay validates if card number is valid and returns answer
+        setDate(state)
+        return state
+    }
 
     return (
         <Fragment>
@@ -20,21 +43,37 @@ const CardForm = ({ continuePayment, emptyInputs }) => {
                 <label>Holder Name:</label>
                 <input data-openpay-card="holder_name" size="50" type="text" />
                 <label>Card number:</label>
-                <input data-openpay-card="card_number" size="50" type="text" />
+                <input
+                    id="card"
+                    data-openpay-card="card_number"
+                    size="50"
+                    type="text"
+                />
+                <p style={card ? { display: 'none' } : { display: 'block' }}>
+                    Invalid Card Number
+                </p>
                 <label>Expiration year:</label>
                 <input
+                    id="year"
                     data-openpay-card="expiration_year"
                     size="4"
                     type="text"
                 />
                 <label>Expiration month:</label>
                 <input
+                    id="month"
                     data-openpay-card="expiration_month"
                     size="4"
                     type="text"
                 />
-                <label>cvv2:</label>
-                <input data-openpay-card="cvv2" size="5" type="text" />
+                <p style={date ? { display: 'none' } : { display: 'block' }}>
+                    Invalid Date
+                </p>
+                <label>CVC:</label>
+                <input id="cvc" data-openpay-card="cvv2" size="5" type="text" />
+                <p style={cvc ? { display: 'none' } : { display: 'block' }}>
+                    Invalid CVC
+                </p>
                 <input
                     id="makeRequestCard"
                     type="button"
